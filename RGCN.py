99a3d98f3a4d234d9eb0df860a2f00ab54ac6174
DESKTOP-75V1S1U\Haeun <haeun5419@a.ut.ac.kr>
2022-03-10 20:39:18 +0900

    0310

--- RGCN.py
+++ RGCN.py
@@ -1,106 +1,150 @@
-import torch
-import torch.nn.functional as F
-from torch_geometric_temporal.nn.recurrent import GConvGRU
-
-import numpy as np
-import pickle as pkl  # serialize를 위한 바이너리 프로토콜 구현. 클래스를 통째로 저장 및 불러올 때 유용하다.
-import networkx as nx  # 그래프를 다루기 위한 파이썬 패키지
-import scipy.sparse as sp  # 행렬의 대부분의 원소가 0인 sparse matrix를 만들기 위해 불러온다
-from scipy.sparse.linalg.eigen.arpack import eigsh  # 대칭행렬에서 k개의 eigen vector와 eigen value를 가져옴
-import sys
-import torch
-from torch import nn
-from torch import optim
-from torch.nn import functional as F
-import numpy as np
-import easydict
+def masked_loss(out, label, mask):
+    '''
+    loss function으로 이용되는 함수이다. mask 벡터의 원소가 True면 1 False면 0이기 때문에 True인 부분의 loss만을 가중치를 주어 계산하고,
+    0인 부분, 즉 False인 부분은 masking이 된 것으로 판단하여 loss 계산에서 제외한다.
+    '''
+    loss = F.cross_entropy(out, label, reduction='none')  # 우선 레이블이 잇는 데이터의 아웃풋과 레이블의 크로스엔트로피로스를 구한다.
+    mask = mask.float()  # 0,1 (False, True의 int형)로 이루어진 벡터로 되어있는 mask를 float으로 바꿔준다.
+    mask = mask / mask.mean()  # 벡터의 각 값을 mask의 평균으로 나눈다.
+    loss *= mask  # mask 벡터에 loss를 곱한다.
+    loss = loss.mean()
+    return loss
 
 
-# easydict를 이용해 hyperparameter 및 사용할 데이터를 미리 정의한다.
-args = easydict.EasyDict({'dataset': 'pubmed',
-                          'model': 'gcn',
-                          'learning_rate': 0.01,
-                          'epochs': 400,
-                          'hidden': 16,
-                          'dropout': 0.5,
-                          'weight_decay': 5e-4,
-                          'early_stopping': 10,
-                          'max_degree': 3
-                          })
-
-names = ['x', 'y', 'tx', 'ty', 'allx', 'ally', 'graph']
-objects = []
-
-for i in range(len(names)):  # 각 파일들을 열고 pickle모듈을 이용해 serialized된 데이터를 파싱한다.
-    with open("data/ind.{}.{}".format(args.dataset, names[i]), 'rb') as f:
-        objects.append(pkl.load(f, encoding='latin1'))
-
-print(len(objects))  # 7이 잘 나온다.
+def masked_acc(out, label, mask):
+    '''
+    accuracy를 구하는 함수이다. 위와 마찬가지로 마스킹이 되지 않은 부분, 즉 마스크 벡터가 True인 부분의 accuracy를 구한다. 
+    '''
+    # [node, f]
+    pred = out.argmax(dim=1)
+    correct = torch.eq(pred, label).float()
+    mask = mask.float()
+    mask = mask / mask.mean()
+    correct *= mask
+    acc = correct.mean()
+    return acc
 
 
-def parse_index_file(filename):  # 테스트데이터의 index를 담고 있는 파일을 파싱하기 위한 함수
-    index = []
-    for line in open(filename):
-        index.append(int(line.strip()))
+def sparse_dropout(x, rate, noise_shape):
+    '''
+    drop-out을 시행하는 함수이다. input matrix와 dropout의 비율과 0이 아닌 값을 가지는 원소의 개수를 argument로 받는다.
+    '''
 
-    return index
+    random_tensor = 1 - rate
+    random_tensor += torch.rand(noise_shape).to(x.device)
+    dropout_mask = torch.floor(random_tensor).byte()
+    i = x._indices()  # [2, 49216]
+    v = x._values()  # [49216]
 
+    # [2, 49216] => [49216, 2] => [remained node, 2] => [2, remained node]
+    i = i[:, dropout_mask]
+    v = v[dropout_mask]
 
-x, y, tx, ty, allx, ally, graph = tuple(objects)  # 튜플로 변환하고 7개의 데이터를 나눈다.
+    out = torch.sparse.FloatTensor(i, v, x.shape).to(x.device)
 
-test_idx_reorder = parse_index_file("data/ind.{}.test.index".format(args.dataset))
-test_idx_range = np.sort(test_idx_reorder)  # 테스트데이터의 index를 오름차순 정렬한다.
+    out = out * (1. / (1 - rate))
 
-features = sp.vstack((allx, tx)).tolil()  # train데이터와 테스트데이터를 하나의 matrix로 결합하고 LInked List format으로 변환
-
-features[test_idx_reorder, :] = features[test_idx_range, :]  # 테스트 데이터의 인덱스를 이용해 테스트 데이터를 전체 메트릭스 안에서 재배치 한다
-adj = nx.adjacency_matrix(nx.from_dict_of_lists(graph))  # node list를 받아 adjacency matrix를 만들어주는 함수이다.
-
-labels = np.vstack((ally, ty))  # 전체 레이블 데이터를 하나의 matrix로 만든다.
-
-labels[test_idx_reorder, :] = labels[test_idx_range, :]  # 테스트데이터의 인덱스를 이용해 테스트 레이블 데이터를 전체 레이블 matrix에서 재배치한다
-
-idx_test = test_idx_range.tolist()
-idx_train = range(len(y))
-idx_val = range(len(y), len(y) + 500)
-
-'''
-이렇게 train, test, validation 데이터 나누기를 위한 준비가 완성되었다.
-'''
+    return out
 
 
-def sample_mask(idx, l):  # 마스크를 만드는 함수
-    # 주어진 데이터가 마스킹이 되어있지 않은 상태이기 때문에 따로 마스킹이 필요하다.
+class GraphConvolution(nn.Module):  # graph convolution을 실행할 함수를 만든다.
 
-    mask = np.zeros(l)  # ㅣ차원의 벡터를 만들고 0으로 채운다.
-    mask[idx] = 1  # 0벡터의 idx 자리에 1을 넣는다. 0은 마스킹을 하는 것이고 1은 마스킹을 하지 않는 것이다.
+    def __init__(self, input_dim, output_dim, num_features_nonzero,
+                 dropout=0.,
+                 is_sparse_inputs=False,
+                 bias=False,
+                 activation=F.relu,
+                 featureless=False):
+        super(GraphConvolution, self).__init__()
 
-    return np.array(mask, dtype=np.bool)
+        self.dropout = dropout
+        self.bias = bias
+        self.activation = activation
+        self.is_sparse_inputs = is_sparse_inputs
+        self.featureless = featureless
+        self.num_features_nonzero = num_features_nonzero
+
+        self.weight = nn.Parameter(torch.randn(input_dim, output_dim))  # weight parameter matrix를 차원에 맞춰 랜덤한 수를 넣어 만든다.
+        self.bias = None
+        if bias:
+            self.bias = nn.Parameter(torch.zeros(output_dim))  # bias parameter matrix를 차원에 맞춰 0을 패딩하여 만든다.
+
+    def forward(self, inputs):
+
+        '''
+        forward()는 모델이 학습데이터를 입력받아서 forward propagation을 진행시키는 함수이고, 반드시 forward 라는 이름의 함수이어야 한다.
+        forward()는 __call__의 역할을 수행하기 때문에, 인스턴스를 만들면 바로 실행된다. model.forward(inputs)가 아닌 model(inputs)로 forward를 진행한다.
+        '''
+
+        x, support = inputs  # support는 classification을 할 때 중심이 되는 벡터
+
+        if self.training and self.is_sparse_inputs:
+            x = sparse_dropout(x, self.dropout, self.num_features_nonzero)
+        elif self.training:
+            x = F.dropout(x, self.dropout)
+
+        # convolve
+        if not self.featureless:  # if it has features x
+            if self.is_sparse_inputs:
+                xw = torch.sparse.mm(x, self.weight)  # sparse matrix 형태의 input * weight
+            else:
+                xw = torch.mm(x, self.weight)
+        else:
+            xw = self.weight
+
+        out = torch.sparse.mm(support, xw)
+
+        if self.bias is not None:
+            out += self.bias  # input * weight + bias
+
+        return self.activation(out), support
 
 
-num_of_labels = labels.shape[0]  # 19717개의 레이블이 있다.
+class GCN(nn.Module):
 
-train_mask = sample_mask(idx_train, num_of_labels)
-val_mask = sample_mask(idx_val, num_of_labels)
-test_mask = sample_mask(idx_test, num_of_labels)
+    def __init__(self, input_dim, output_dim, num_features_nonzero):
+        super(GCN, self).__init__()
 
-y_train = np.zeros(labels.shape)  # labels.shape는 (19717, 3) 이다.
-y_val = np.zeros(labels.shape)
-y_test = np.zeros(labels.shape)
+        self.input_dim = input_dim  # 1433
+        self.output_dim = output_dim
 
-# label data를 마스킹한 후 재구성한다. Semi-Supervised이기 때문에 마스킹을 해주는 것이다.
+        print('input dim:', input_dim)
+        print('output dim:', output_dim)
+        print('num_features_nonzero:', num_features_nonzero)
 
+        self.layers = nn.Sequential(GraphConvolution(self.input_dim, args.hidden, num_features_nonzero,
+                                                     activation=F.relu,
+                                                     dropout=args.dropout,
+                                                     is_sparse_inputs=True),
 
-y_train[train_mask, :] = labels[train_mask, :]
-y_val[val_mask, :] = labels[val_mask, :]
-y_test[test_mask, :] = labels[test_mask, :]
+                                    GraphConvolution(args.hidden, output_dim, num_features_nonzero,
+                                                     activation=F.relu,
+                                                     dropout=args.dropout,
+                                                     is_sparse_inputs=False),
 
-seed = 123
-np.random.seed(seed)
-torch.random.manual_seed(seed)
+                                    )
 
-# 이제 차원이 제대로 맞추어져 있는지 확인해보자
-print('adj:', adj.shape)
-print('features:', features.shape)
-print('y:', y_train.shape, y_val.shape, y_test.shape)
-print('mask:', train_mask.shape, val_mask.shape, test_mask.shape)
+    def forward(self, inputs):
+
+        x, support = inputs
+
+        x = self.layers((x, support))
+        '''
+        inputs라는 하나의 arg를 받기때문에 x, support를 튜플로 묶어서 input으로 넣고 GraphConvoltion class 의 forward를 실행시킨다.
+        '''
+        return x
+
+    def l2_loss(self):  # weight^2을 regularization으로 준다.
+
+        layer = self.layers.children()  # gives an iterator over the layers in network model.
+        layer = next(iter(layer))  # 레이어를 iterate하게 꺼낸다.
+
+        loss = None
+
+        for p in layer.parameters():  # 레이어의 weight를 제곱하여 loss에 더한다.
+            if loss is None:
+                loss = p.pow(2).sum()
+            else:
+                loss += p.pow(2).sum()
+
+        return loss
---
+++ standaloneGCN.py
@@ -0,0 +1,291 @@
+import argparse
+import sys
+import time
+import copy
+
+import numpy as np
+
+from rdkit import Chem, DataStructs
+from rdkit.Chem import AllChem
+from rdkit.Chem.Crippen import MolLogP
+
+from sklearn.metrics import mean_absolute_error
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+from torch.autograd import Variable
+
+from tqdm import tnrange, tqdm_notebook
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+''' GCNLayer: node feature matrix와 adjacency matrix의 list를 받아 graph convolution 연산을 수행하는 module '''
+class GCNLayer(nn.Module):
+
+    def __init__(self, in_dim, out_dim, n_atom, act=None, bn=False):
+        super(GCNLayer, self).__init__()
+
+        self.use_bn = bn
+        self.linear = nn.Linear(in_dim, out_dim)
+        nn.init.xavier_uniform_(self.linear.weight)
+        self.bn = nn.BatchNorm1d(n_atom)
+        self.activation = act
+
+    def forward(self, x, adj):
+        out = self.linear(x)
+        out = torch.matmul(adj, out)
+        if self.use_bn:
+            out = self.bn(out)
+        if self.activation != None:
+            out = self.activation(out)
+        return out, adj
+
+''' (Gated)SkipConnection: ResNet에서 사용되었던 skip connection technique을 구현한 module  '''
+class SkipConnection(nn.Module):
+
+    def __init__(self, in_dim, out_dim):
+        super(SkipConnection, self).__init__()
+
+        self.in_dim = in_dim
+        self.out_dim = out_dim
+
+        self.linear = nn.Linear(in_dim, out_dim, bias=False)
+
+    def forward(self, in_x, out_x):
+        if (self.in_dim != self.out_dim):
+            in_x = self.linear(in_x)
+        out = in_x + out_x
+        return out
+
+
+
+
+class GatedSkipConnection(nn.Module):
+
+    def __init__(self, in_dim, out_dim):
+        super(GatedSkipConnection, self).__init__()
+
+        self.in_dim = in_dim
+        self.out_dim = out_dim
+
+        self.linear = nn.Linear(in_dim, out_dim, bias=False)
+        self.linear_coef_in = nn.Linear(out_dim, out_dim)
+        self.linear_coef_out = nn.Linear(out_dim, out_dim)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, in_x, out_x):
+        if (self.in_dim != self.out_dim):
+            in_x = self.linear(in_x)
+        z = self.gate_coefficient(in_x, out_x)
+        out = torch.mul(z, out_x) + torch.mul(1.0 - z, in_x)
+        return out
+
+    def gate_coefficient(self, in_x, out_x):
+        x1 = self.linear_coef_in(in_x)
+        x2 = self.linear_coef_out(out_x)
+        return self.sigmoid(x1 + x2)
+
+'''GCNBlock: node feature matrix와 adjacency matrix의 list를 받아 원하는 갯수의 GCNLayer를 통과시킨 후, (gated)skip connection을 적용하는 module'''
+class GCNBlock(nn.Module):
+
+    def __init__(self, n_layer, in_dim, hidden_dim, out_dim, n_atom, bn=True, sc='gsc'):
+        super(GCNBlock, self).__init__()
+
+        self.layers = nn.ModuleList()
+        for i in range(n_layer):
+            self.layers.append(GCNLayer(in_dim if i == 0 else hidden_dim,
+                                        out_dim if i == n_layer - 1 else hidden_dim,
+                                        n_atom,
+                                        nn.ReLU() if i != n_layer - 1 else None,
+                                        bn))
+        self.relu = nn.ReLU()
+        if sc == 'gsc':
+            self.sc = GatedSkipConnection(in_dim, out_dim)
+        elif sc == 'sc':
+            self.sc = SkipConnection(in_dim, out_dim)
+        elif sc == 'no':
+            self.sc = None
+        else:
+            assert False, "Wrong sc type."
+
+    def forward(self, x, adj):
+        residual = x
+        for i, layer in enumerate(self.layers):
+            out, adj = layer((x if i == 0 else out), adj)
+        if self.sc != None:
+            out = self.sc(residual, out)
+        out = self.relu(out)
+        return out, adj
+
+
+'''ReadOut: graph structrure에 permutation invariance를 주기 위하여 linear layer를 거친 뒤 batch 별로 summation하는 module'''
+class ReadOut(nn.Module):
+
+    def __init__(self, in_dim, out_dim, act=None):
+        super(ReadOut, self).__init__()
+
+        self.in_dim = in_dim
+        self.out_dim = out_dim
+
+        self.linear = nn.Linear(self.in_dim,
+                                self.out_dim)
+        nn.init.xavier_uniform_(self.linear.weight)
+        self.activation = act
+
+    def forward(self, x):
+        out = self.linear(x)
+        out = torch.sum(out, 1)
+        if self.activation != None:
+            out = self.activation(out)
+        return out
+
+'''Predictor: ReadOut layer로부터의 graph feature vector로부터 logP value를 예측하기 위한 linear layer module'''
+class Predictor(nn.Module):
+
+    def __init__(self, in_dim, out_dim, act=None):
+        super(Predictor, self).__init__()
+
+        self.in_dim = in_dim
+        self.out_dim = out_dim
+
+        self.linear = nn.Linear(self.in_dim,
+                                self.out_dim)
+        nn.init.xavier_uniform_(self.linear.weight)
+        self.activation = act
+
+    def forward(self, x):
+        out = self.linear(x)
+        if self.activation != None:
+            out = self.activation(out)
+        return out
+
+
+class GCNNet(nn.Module):
+
+    def __init__(self, args):
+        super(GCNNet, self).__init__()
+
+        self.blocks = nn.ModuleList()
+        for i in range(args.n_block):
+            self.blocks.append(GCNBlock(args.n_layer,
+                                        args.in_dim if i == 0 else args.hidden_dim,
+                                        args.hidden_dim,
+                                        args.hidden_dim,
+                                        args.n_atom,
+                                        args.bn,
+                                        args.sc))
+        self.readout = ReadOut(args.hidden_dim,
+                               args.pred_dim1,
+                               act=nn.ReLU())
+        self.pred1 = Predictor(args.pred_dim1,
+                               args.pred_dim2,
+                               act=nn.ReLU())
+        self.pred2 = Predictor(args.pred_dim2,
+                               args.pred_dim3,
+                               act=nn.Tanh())
+        self.pred3 = Predictor(args.pred_dim3,
+                               args.out_dim)
+
+    def forward(self, x, adj):
+        for i, block in enumerate(self.blocks):
+            out, adj = block((x if i == 0 else out), adj)
+        out = self.readout(out)
+        out = self.pred1(out)
+        out = self.pred2(out)
+        out = self.pred3(out)
+        return out
+
+
+def train(net, partition, optimizer, criterion, args):
+    trainloader = torch.utils.data.DataLoader(partition['train'],
+                                              batch_size=args.train_batch_size,
+                                              shuffle=True, num_workers=2)
+    net.train()
+
+    train_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        optimizer.zero_grad() # [21.01.05 오류 수정] 매 Epoch 마다 .zero_grad()가 실행되는 것을 매 iteration 마다 실행되도록 수정했습니다.
+
+        # get the inputs
+        list_feature, list_adj, list_logP = data
+        list_feature = list_feature.cuda().float()
+        list_adj = list_adj.cuda().float()
+        list_logP = list_logP.cuda().float().view(-1, 1)
+        outputs = net(list_feature, list_adj)
+
+        loss = criterion(outputs, list_logP)
+        train_loss += loss.item()
+        loss.backward()
+        optimizer.step()
+
+    train_loss = train_loss / len(trainloader)
+    return net, train_loss
+
+
+def test(net, partition, args):
+    testloader = torch.utils.data.DataLoader(partition['test'],
+                                             batch_size=args.test_batch_size,
+                                             shuffle=False, num_workers=2)
+    net.eval()
+    with torch.no_grad():
+        logP_total = list()
+        pred_logP_total = list()
+        for data in testloader:
+            list_feature, list_adj, list_logP = data
+            list_feature = list_feature.cuda().float()
+            list_adj = list_adj.cuda().float()
+            list_logP = list_logP.cuda().float()
+            logP_total += list_logP.tolist()
+            list_logP = list_logP.view(-1, 1)
+
+            outputs = net(list_feature, list_adj)
+            pred_logP_total += outputs.view(-1).tolist()
+
+        mae = mean_absolute_error(logP_total, pred_logP_total)
+        std = np.std(np.array(logP_total) - np.array(pred_logP_total))
+
+    return mae, std, logP_total, pred_logP_total
+
+
+def experiment(partition, args):
+    net = GCNNet()
+    net.cuda()
+
+    criterion = nn.MSELoss()
+    if args.optim == 'SGD':
+        optimizer = optim.SGD(net.parameters(), lr=args.lr, weight_decay=args.l2)
+    elif args.optim == 'RMSprop':
+        optimizer = optim.RMSprop(net.parameters(), lr=args.lr, weight_decay=args.l2)
+    elif args.optim == 'Adam':
+        optimizer = optim.Adam(net.parameters(), lr=args.lr, weight_decay=args.l2)
+    else:
+        raise ValueError('In-valid optimizer choice')
+
+    train_losses = []
+    val_losses = []
+
+    for epoch in range(args.epoch):  # loop over the dataset multiple times
+        ts = time.time()
+        net, train_loss = train(net, partition, optimizer, criterion, args)
+        val_loss = validate(net, partition, criterion, args)
+        te = time.time()
+
+        train_losses.append(train_loss)
+        val_losses.append(val_loss)
+
+        print('Epoch {}, Acc(train/val): {:2.2f}/{:2.2f}, Loss(train/val) {:2.2f}/{:2.2f}. Took {:2.2f} sec'.format(epoch,train_acc,val_acc,train_loss,val_loss,te - ts))
+
+    mae, std, logP_total, pred_logP_total = test(net, partition, args)
+
+    result = {}
+    result['train_losses'] = train_losses
+    result['val_losses'] = val_losses
+    result['mae'] = mae
+    result['std'] = std
+    result['logP_total'] = logP_total
+    result['pred_logP_total'] = pred_logP_total
+    return vars(args), result
